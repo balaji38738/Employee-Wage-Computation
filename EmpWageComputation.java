72d80fd1edcbac1bbd27d44885a83d3c2dde2aa3
balaji38738 <bijjapwar123@gmail.com>
2020-03-29 01:05:07 +0530

    Used switch case to decide employee hours

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,23 +1,28 @@
 public class EmpWageComputation {
 
+	//CONSTANTS
+	public static final int IS_FULL_TIME = 1, IS_PART_TIME = 2;
+	public static final int EMP_RATE_PER_HOUR = 20;
+	
 	public static void main(String[] args) {
 		
 		System.out.println("Welcome to Employee Wage Computation");
-		//CONSTANTS
-		int IS_FULL_TIME = 1, IS_PART_TIME = 2;
-		int EMP_RATE_PER_HOUR = 20;
 
 		//Variables
 		int empHrs = 0, empWage = 0;
 
 		//Computation
 		int empCheck = ((int) Math.floor(Math.random() * 10)) % 3;
-		if (empCheck == IS_FULL_TIME)
-			empHrs = 8;
-		else if (empCheck == IS_PART_TIME)
-			empHrs = 4;
-		else
-			empHrs = 0;
+		switch(empCheck) {
+			case IS_FULL_TIME:
+				empHrs = 8;
+				break;
+			case IS_PART_TIME:
+				empHrs = 4;
+				break;
+			default:
+				empHrs = 0;
+		}
 		empWage = empHrs * EMP_RATE_PER_HOUR;
 		System.out.println("Employee wage: "+empWage);
  
