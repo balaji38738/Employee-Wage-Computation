2ae615808515034fb6a40af33ef5b0b5afc37a37
balaji38738 <bijjapwar123@gmail.com>
2020-03-29 00:41:04 +0530

    Calculated full time employee wage

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -5,14 +5,18 @@ public class EmpWageComputation {
 		System.out.println("Welcome to Employee Wage Computation");
 		//CONSTANTS
 		int IS_FULL_TIME = 1;
+		int EMP_RATE_PER_HOUR = 20;
+
+		//Variables
+		int empHrs = 0, empWage = 0;
 
 		//Computation
 		int empCheck = ((int) Math.floor(Math.random() * 10)) % 2;
 		if(empCheck == IS_FULL_TIME)
-			System.out.println("Employee is present");
-		else
-			System.out.println("Employee is absent");
-
+			empHrs = 8;
+		empWage = empHrs * EMP_RATE_PER_HOUR;
+		System.out.println("Employee wage: "+empWage);
+ 
 	}
 
 }
