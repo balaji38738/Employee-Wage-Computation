28c86ada08bae83fd81756c921db35cde600f7b5
balaji38738 <bijjapwar123@gmail.com>
2020-03-29 00:50:14 +0530

    Calculated part time employee wage

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -4,16 +4,20 @@ public class EmpWageComputation {
 		
 		System.out.println("Welcome to Employee Wage Computation");
 		//CONSTANTS
-		int IS_FULL_TIME = 1;
+		int IS_FULL_TIME = 1, IS_PART_TIME = 2;
 		int EMP_RATE_PER_HOUR = 20;
 
 		//Variables
 		int empHrs = 0, empWage = 0;
 
 		//Computation
-		int empCheck = ((int) Math.floor(Math.random() * 10)) % 2;
-		if(empCheck == IS_FULL_TIME)
+		int empCheck = ((int) Math.floor(Math.random() * 10)) % 3;
+		if (empCheck == IS_FULL_TIME)
 			empHrs = 8;
+		else if (empCheck == IS_PART_TIME)
+			empHrs = 4;
+		else
+			empHrs = 0;
 		empWage = empHrs * EMP_RATE_PER_HOUR;
 		System.out.println("Employee wage: "+empWage);
  
