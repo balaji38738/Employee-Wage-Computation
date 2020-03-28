f8627e21a7924f3d1716409117827ff0620ee971
balaji38738 <bijjapwar123@gmail.com>
2020-03-29 00:27:08 +0530

    Checking employee presence

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -3,6 +3,16 @@ public class EmpWageComputation {
 	public static void main(String[] args) {
 		
 		System.out.println("Welcome to Employee Wage Computation");
+		//CONSTANTS
+		int IS_FULL_TIME = 1;
+
+		//Computation
+		int empCheck = ((int) Math.floor(Math.random() * 10)) % 2;
+		if(empCheck == IS_FULL_TIME)
+			System.out.println("Employee is present");
+		else
+			System.out.println("Employee is absent");
 
 	}
+
 }
