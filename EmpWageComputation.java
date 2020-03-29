6b61875aa1fcabeb22d09809ee115aac22966e26
balaji38738 <bijjapwar123@gmail.com>
2020-03-30 03:47:57 +0530

    Calculated employee wage for multiple companies

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,46 +1,16 @@
-interface Computable {
-
-	void computeEmpWage();
-
-}
-
-public class EmpWageComputation implements Computable {
+public class EmpWageComputation {
 
 	//CONSTANTS
 	private static final int IS_FULL_TIME = 1, IS_PART_TIME = 2;
-	private final int TOTAL_COMPANIES;
+	private static final int TOTAL_COMPANIES = 10;
 
 	//Variables
 	private static int empRatePerHour = 0, numWorkingDays = 0;
 	private static int maxHrsInMonth = 0;
-	CompanyEmpWage employee[];
-
-	public EmpWageComputation(int totalCompanies) {
-
-		this.TOTAL_COMPANIES = totalCompanies;
-		this.employee = new CompanyEmpWage [this.TOTAL_COMPANIES];
-
-	}
 
 	public static void main(String[] args) {
 
 		System.out.println("Welcome to Employee Wage Computation\n");
-<<<<<<< HEAD
-		EmpWageComputation firstComp = new EmpWageComputation(20);
-	
-		//Generate Employee rate between 100 to 300
-		empRatePerHour = (int) (100 + Math.random() * 300);
-
-		//Generate Employee working days between 20 to 25
-		numWorkingDays = (int) (20 + Math.random() * 25);
-
-		//Generate Employee work hours between 100 to 200
-		maxHrsInMonth = (int) (100 + Math.random() * 200);
-
-		for (int i = 0; i < firstComp.employee.length; i++) {
-			firstComp.employee[i] = new CompanyEmpWage(empRatePerHour,
-			                        numWorkingDays, maxHrsInMonth);
-=======
 
 		CompanyEmpWage employee[] = new CompanyEmpWage [TOTAL_COMPANIES];
 		for (int i = 0; i < TOTAL_COMPANIES; i++) {
@@ -55,42 +25,38 @@ public class EmpWageComputation implements Computable {
 			employee[i] = new CompanyEmpWage(empRatePerHour, numWorkingDays,
 											 maxHrsInMonth);
 			computeEmpWage(employee[i], i);
->>>>>>> UC10-CompanyEmpWage
 		}
-		firstComp.computeEmpWage();
 
 	}
 
 	//Calculates employee wage
-	public void computeEmpWage() {
+	public static void computeEmpWage(CompanyEmpWage employee, int empNo) {
 
-		for (int empNo = 0; empNo < employee.length; empNo++)
-		{
-			int empHrs = 0, empWage = 0, totalEmpWage = 0;
-			int totalWorkingDays = 0, totalEmpHrs = 0;
+		int empHrs = 0, empWage = 0;
+		int totalWorkingDays = 0, totalEmpHrs = 0;
+		int totalEmpWage = 0;
 
-			//Computation
-			while (totalEmpHrs <= employee[empNo].getMaxHrsInMonth() &&
-			       totalWorkingDays < employee[empNo].getNumWorkingDays()) {
-				totalWorkingDays++;
-				int empCheck = ((int) Math.floor(Math.random() * 10)) % 3;
-				switch(empCheck) {
-					case IS_FULL_TIME:
-						empHrs = 8;
-						break;
-					case IS_PART_TIME:
-						empHrs = 4;
-						break;
-					default:
-						empHrs = 0;
-				}
-				totalEmpHrs += empHrs;
-				empWage = empHrs * employee[empNo].getEmpRatePerHour();
-				totalEmpWage += empWage;
+		//Computation
+		while (totalEmpHrs <= employee.getMaxHrsInMonth() &&
+		       totalWorkingDays < employee.getNumWorkingDays()) {
+			totalWorkingDays++;
+			int empCheck = ((int) Math.floor(Math.random() * 10)) % 3;
+			switch(empCheck) {
+				case IS_FULL_TIME:
+					empHrs = 8;
+					break;
+				case IS_PART_TIME:
+					empHrs = 4;
+					break;
+				default:
+					empHrs = 0;
 			}
-			employee[empNo].setTotalEmpWage(totalEmpWage);
-	 		System.out.println("Monthly Wage of employee " + (empNo + 1) + ":" + totalEmpWage);
-	 	}
+			totalEmpHrs += empHrs;
+			empWage = empHrs * employee.getEmpRatePerHour();
+			totalEmpWage += empWage;
+		}
+		employee.setTotalEmpWage(totalEmpWage);
+ 		System.out.println("Monthly Wage of employee " + (empNo + 1) + ":" + totalEmpWage);
 
 	}
 
@@ -105,13 +71,13 @@ class CompanyEmpWage {
 	//Variables
 	private int totalEmpWage = 0;
 
-	public CompanyEmpWage(int empRatePerHour,
-						  int numWorkingDays,
-						  int maxHrsInMonth) {
+	public CompanyEmpWage(int EMP_RATE_PER_HOUR,
+						  int NUM_WORKING_DAYS,
+						  int MAX_HRS_IN_MONTH) {
 
-		this.EMP_RATE_PER_HOUR = empRatePerHour;
-		this.NUM_WORKING_DAYS = numWorkingDays;
-		this.MAX_HRS_IN_MONTH = maxHrsInMonth;
+		this.EMP_RATE_PER_HOUR = EMP_RATE_PER_HOUR;
+		this.NUM_WORKING_DAYS = NUM_WORKING_DAYS;
+		this.MAX_HRS_IN_MONTH = MAX_HRS_IN_MONTH;
 
 	}
 
