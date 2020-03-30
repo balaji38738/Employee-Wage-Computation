e79e3d215a38e03ea80dd15a9b93707f89db2fff
balaji38738 <bijjapwar123@gmail.com>
2020-03-31 02:54:40 +0530

    Fixed bugs in generating employee variables

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -29,13 +29,13 @@ public class EmpWageComputation implements Computable {
 		for (int i = 0; i < firstComp.employee.length; i++) {
 				
 			//Generate Employee rate between 100 to 300
-			empRatePerHour = (int) (100 + Math.random() * 300);
+			empRatePerHour = (int) (100 + Math.random() * 200);
 
 			//Generate Employee working days between 20 to 25
-			numWorkingDays = (int) (20 + Math.random() * 25);
+			numWorkingDays = (int) (20 + Math.random() * 5);
 
 			//Generate Employee work hours between 100 to 200
-			maxHrsInMonth = (int) (100 + Math.random() * 200);
+			maxHrsInMonth = (int) (100 + Math.random() * 100);
 
 			firstComp.employee[i] = new CompanyEmpWage(empRatePerHour,
 			                        numWorkingDays, maxHrsInMonth);
